8f13a66f2d296e7ced64b7c67e50cf31c6a1efd8
gabo <gabrielbarragan07@gmail.com>
2021-12-17 22:20:54 -0500

    Resuelto el reto de decoradores, se crea el archivo decoradores_reto.py y se crea un decorador para hacer llevar un log de funciones

--- decoradores_reto.py
+++ decoradores_reto.py
@@ -1,1 +1,27 @@
-def 
+from datetime import datetime
+
+def logger(file_log):
+    """ Decorator to capture the execution time of a function."""
+    def decorator_log(func):
+
+        def wrapper(*args, **kwargs):
+            """Writes to a file the executed function and the time at which it was executed"""
+            with open(file_log, 'a') as opened_file:
+                time_log = datetime.now()
+                func_output = func.__name__
+                output = func(*args, **kwargs)
+                opened_file.write(f'{time_log}: {func_output}\n')
+                return output
+
+        return wrapper
+    return decorator_log
+    
+@logger('logger.log')
+def suma(a,b):
+    return a + b
+
+def run():
+    print(suma(1,5))
+
+if '__main__' == __name__:
+    run()
